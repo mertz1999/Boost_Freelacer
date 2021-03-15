a95425c92de464ae6ca9e4fd508ea1facec99c80
Reza Tanakizadeh <reza.tz780210@gmail.com>
2021-03-15 18:12:17 +0330

    24 esfand 99

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # --- import needed libraries
 from bs4 import BeautifulSoup
 import requests
+import get_skills as gs
 
 # --- request to a website
-html_text = requests.get('https://ponisha.ir/search/projects').text
+html_text = requests.get('https://ponisha.ir/search/projects/skill-%D8%B1%DB%8C%D8%A7%DA%A9%D8%AA-%D9%86%DB%8C%D8%AA%DB%8C%D9%88/currency-IRR/status-open/sort-newest').text
 
 # --- instance soup
 soup = BeautifulSoup(html_text, 'lxml')
@@ -21,7 +22,24 @@ for project in projects:
     temp = project.find_all('a', class_='no-link-inherit')
     skills.append([i['title'] for i in temp])
 
-print(titles[1])
-print(skills[1])
+# --- replace '\u200c' in persian font to a space
+temp = []
+for skill in skills:
+    temp1 = []
+    for j in skill:
+        temp2 = j.replace('\u200c', " ")
+        temp1.append(temp2)
+    temp.append(temp1)
 
-# OK
+skills []
+skills = temp
+
+
+#
+# my_skills = gs.my_skills_list
+#
+# for index, skill in enumerate(skills):
+#     for i in my_skills:
+#         if i in skill:
+#             print(index)
+#             break
